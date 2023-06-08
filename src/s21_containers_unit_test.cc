7131fa7360cafad88f8c43d213c5f22aca1a0472
Gleb Tolstenev <yonnarge@student.21-school.ru>
2023-06-08 03:30:17 +0300

    Improve separate tests in Makefile abd little fixes.

--- src/s21_containers_unit_test.cc
+++ src/s21_containers_unit_test.cc
@@ -2,43 +2,34 @@
 #include "s21_containers.h"
 #include "s21_containersplus.h"
 
-// Все тесты на контейнер должны начинаться с одного и того же названия.
-// Например, Array. Это нужно для того, чтобы они были объединены в один
+// Все тесты на контейнер должны начинаться с названия контейнера,
+// например, Array. Это нужно для того, чтобы они были объединены в один
 // test suite и их можно было раздельно запускать make test_array
 TEST(Array, EmptyTest1) {
-
 }
 
 TEST(Array, EmptyTest2) {
-
 }
 
 TEST(List, EmptyTest) {
-
 }
 
 TEST(Map, EmptyTest) {
-
 }
 
 TEST(Multiset, EmptyTest) {
-
 }
 
 TEST(Queue, EmptyTest) {
-
 }
 
 TEST(Set, EmptyTest) {
-
 }
 
 TEST(Stack, EmptyTest) {
-
 }
 
 TEST(Vector, EmptyTest) {
-
 }
 
 int main(int argc, char **argv) {
