1cda447b394c350016c222aa94ef4a054cdcaab0
Gleb Tolstenev <yonnarge@student.21-school.ru>
2023-07-19 21:53:37 +0300

    Add includes into test_s21_containers.cc

--- src/tests/test_s21_containers.cc
+++ src/tests/test_s21_containers.cc
@@ -1,6 +1,13 @@
 #include "test_s21_containers.h"
 
+#include "test_s21_array.cc"
 #include "test_s21_list.cc"
+#include "test_s21_map.cc"
+#include "test_s21_multiset.cc"
+#include "test_s21_queue.cc"
+#include "test_s21_set.cc"
+#include "test_s21_stack.cc"
+#include "test_s21_vector.cc"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
