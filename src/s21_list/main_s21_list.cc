9fad6e55ba9765650558caeaa2a043d6afcc76d1
Gleb Tolstenev <yonnarge@student.21-school.ru>
2023-07-19 18:15:40 +0300

    Move method realizations from class to header file. Add class List Iterator with two methods: begin() and end(). Add main_s21_list.cc for simple testing.

---
+++ src/s21_list/main_s21_list.cc
@@ -0,0 +1,19 @@
+#include "s21_list.h"
+#include <iostream>
+
+// Этот файл предназначен для проверки кода в main
+
+int main() {
+  s21::List<int> l;
+
+ l.push_back(4);
+ l.push_back(8);
+
+ s21::List<int>::iterator it1 = l.begin();
+ s21::List<int>::iterator it = l.end();
+
+ std::cout << *it1 << std::endl;
+ std::cout << *it << std::endl;
+
+  return 0;
+}
--- src/s21_list/s21_list.h
+++ src/s21_list/s21_list.h
@@ -2,43 +2,96 @@
 #define LIB_CONTAINERS_S21_LIST_H
 
 namespace s21 {
-template <typename T>
-class List {
-public:
+
+template<typename T>
+struct ListNode {
+ public:
   using value_type = T;
-  using reference = T&;
-  using const_reference = const T&;
 
-  List() {
-    end_ = new ListNode;
-    end_->data = nullptr;
-    end_->prev = end_;
-    end_->next = end_;
-  }
-  ~List() {  // Временное решение, течёт. Переписать, когда будет итератор листа
-    delete end_;
-  }
+  value_type *data_{};
+  ListNode *prev_{};
+  ListNode *next_{};
+};
 
-  void push_back(value_type data) {
-    ListNode *last_node = end_->prev;
-    ListNode *node = new ListNode;
-    node->data = new value_type;
-    *(node->data) = data;
-    node->prev = last_node;
-    node->next = end_;
-    end_->prev = node;
-    last_node->next = node;
-  }
+template<typename T>
+class List {
+  class ListIterator;
+ public:  // class List
+  using value_type = T;
+  using reference = T &;
+  using node = ListNode<T>;
+  using iterator = ListIterator;
 
-//private:
-  struct ListNode {
-    value_type *data;
-    ListNode *prev;
-    ListNode *next;
-  };
-  ListNode *end_;
+  List();
+  ~List();
 
+  void push_back(value_type data);
+
+  iterator begin();
+  iterator end();
+
+ private:  // class List
+  node *end_{};
+//  node *begin_ = end_ ? end_->next_ : nullptr;
+
+  class ListIterator {
+   public:  // class ListIterator
+    friend node;
+    ListIterator() = default;
+    explicit ListIterator(node *p) : ptr_(p) {};
+    ListIterator(const ListIterator &other) : ptr_(other.ptr_) {};
+
+    reference operator*() { return *(ptr_->data_); }
+
+   private:  // class ListIterator
+    node *ptr_{};
+  };  // class ListIterator
 };  // class List
+
+
+template<typename T>
+List<T>::List() {
+  end_ = new node;
+  end_->data_ = nullptr;
+  end_->prev_ = end_;
+  end_->next_ = end_;
+}
+
+
+template <class value_type>
+List<value_type>::~List() {  // Временное решение, течёт. Переписать, когда
+  // будет итератор листа
+  if (this->end_) delete end_;
+}
+
+
+template <class value_type>
+void List<value_type>::push_back(value_type data) {
+  node *last_node = end_->prev_;
+  auto *new_node = new node;
+  new_node->data_ = new value_type;
+  *(new_node->data_) = data;
+  new_node->prev_ = last_node;
+  new_node->next_ = end_;
+  end_->prev_ = new_node;
+  last_node->next_ = new_node;
+}
+
+
+template <class value_type>
+typename List<value_type>::iterator List<value_type>::begin() {
+  return iterator(end_->next_);
+}
+
+
+template <class value_type>
+typename List<value_type>::iterator List<value_type>::end() {
+  return iterator(end_->prev_);
+}
+
 }  // namespace s21
 
 #endif //LIB_CONTAINERS_S21_LIST_H
+
+
+
